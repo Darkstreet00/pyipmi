3ac79ac875beae1ea6f30629e0a3c86490f7e72d
darkstreet <55440418+Darkstreet00@users.noreply.github.com>
2021-05-25 00:06:49 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -12,17 +12,14 @@ def color_text(color, text):
     colored_text = f"\033[{color}{text}\033[00m"
     return colored_text
 
-ip = input("IP/DOMAIN: ").encode("utf-8")
-user = input("User: ").encode("utf-8")
+ip = input("IP/DOMAIN: ")
+user = input("User: ")
 passwd = input("Enter your password: ").encode("utf-8")
 
 hashedpasswd = bcrypt.hashpw(passwd, bcrypt.gensalt()).decode("utf-8")
 
 print(color_text(GREEN, "Password hash generated."))
 time.sleep(0.1)
-
-ip = ip.decode("utf-8")
-user = user.decode("utf-8")
 
 config = {
 "ip": ip,
@@ -39,5 +36,3 @@ print(color_text(GREEN, "Config file created."))
 
 time.sleep(0.5)
 sys.exit()
-
-
